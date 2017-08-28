4dc2cd37426612b825cbd6f15550fbaf474affef
Purushottam Kumar <purushottam@gosharp.in>
2017-08-28 09:56:46 +0530

    Initial Commit

---
+++ OTPDemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OTPDemoTest
+//
+//  Created by Purushottam Kumar on 28/08/17.
+//  Copyright © 2017 Purushottam Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OTPDemoTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OTPDemoTest
+//
+//  Created by Purushottam Kumar on 28/08/17.
+//  Copyright © 2017 Purushottam Kumar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
